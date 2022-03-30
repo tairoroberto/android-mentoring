61680750a69f06676324a309c10f7545916a5f79
tairo.toberto <tairoroberto@hotmail.com>
2022-03-30 15:35:53 -0300

    Add register and bing for views

--- projects/HelloWorldKotlin/app/src/main/java/br/com/tairoroberto/helloworldkotlin/LoginActivity.kt
+++ projects/HelloWorldKotlin/app/src/main/java/br/com/tairoroberto/helloworldkotlin/LoginActivity.kt
@@ -1,31 +1,34 @@
 package br.com.tairoroberto.helloworldkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 
 class LoginActivity : AppCompatActivity() {
 
-    lateinit var btnLogin: AppCompatButton
-    lateinit var editTextEmail: EditText
-    lateinit var editTextPassword: EditText
+    private val btnLogin: AppCompatButton by lazy { findViewById(R.id.btnLogin) }
+    private val btnRegister: AppCompatButton by lazy { findViewById(R.id.btnRegister) }
+    private val editTextEmail: EditText by lazy { findViewById(R.id.editTextEmail) }
+    private val editTextPassword: EditText by lazy { findViewById(R.id.editTextPassword) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        btnLogin = findViewById(R.id.btnLogin)
-        editTextEmail = findViewById(R.id.editTextEmail)
-        editTextPassword = findViewById(R.id.editTextPassword)
-
-        btnLogin.setOnClickListener {
+        btnRegister.setOnClickListener {
 
             val email = editTextEmail.text.toString()
             val password = editTextPassword.text.toString()
 
-            Log.i("TAG", "FAZENDO LOGIN: my email -> $email my password -> $password")
+            val intent  = Intent(this, RegisterActivity::class.java).apply {
+                putExtra("EMAIL", email)
+                putExtra("PASSWORD", password)
+            }
+
+            startActivity(intent)
         }
     }
 }
--- projects/HelloWorldKotlin/app/src/main/java/br/com/tairoroberto/helloworldkotlin/RegisterActivity.kt
+++ projects/HelloWorldKotlin/app/src/main/java/br/com/tairoroberto/helloworldkotlin/RegisterActivity.kt
@@ -2,10 +2,23 @@ package br.com.tairoroberto.helloworldkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import br.com.tairoroberto.helloworldkotlin.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.editTextEmail.setText(intent.getStringExtra("EMAIL"))
+        binding.editTextPassword.setText(intent.getStringExtra("PASSWORD"))
+
+
+        binding.btnRegister.setOnClickListener {
+            Log.i("TAG", "Email: ${binding.editTextEmail.text} -> Password: ${binding.editTextPassword.text}")
+        }
     }
 }
