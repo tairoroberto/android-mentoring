7c10d3165f6acb5fea2f5c1a1efd64ad111dc053
tairo.toberto <tairoroberto@hotmail.com>
2022-05-19 14:47:08 -0300

    Change name of classes and add retrofit dependencies

--- projects/HelloWorldKotlin/app/src/main/java/br/com/tairoroberto/helloworldkotlin/home/models/MoviesResponse.kt
+++ projects/HelloWorldKotlin/app/src/main/java/br/com/tairoroberto/helloworldkotlin/home/models/MoviesResponse.kt
@@ -2,11 +2,13 @@ package br.com.tairoroberto.helloworldkotlin.home.models
 
 import com.google.gson.annotations.SerializedName
 
-data class MoviesResponse(@SerializedName("page")
-                          val page: Int = 0,
-                          @SerializedName("total_pages")
-                          val totalPages: Int = 0,
-                          @SerializedName("results")
-                          val results: List<ResultsItem>?,
-                          @SerializedName("total_results")
-                          val totalResults: Int = 0)
+data class MoviesResponse(
+    @SerializedName("page")
+    val page: Int = 0,
+    @SerializedName("total_pages")
+    val totalPages: Int = 0,
+    @SerializedName("results")
+    val movies: List<Movie>?,
+    @SerializedName("total_results")
+    val totalResults: Int = 0
+)
